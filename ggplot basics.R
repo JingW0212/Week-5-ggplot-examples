6005db4bf932fbe1b765b9ef5b3a457234cf013b
mfridson <mf3124@columbia.edu>
2017-02-18 10:16:08 -0500

    Update ggplot basics.R

--- ggplot basics.R
+++ ggplot basics.R
@@ -1,3 +1,53 @@
+#Class Exercises Week 5 ggplot
+
+library(tidyverse)
+#mpg is a data set in the R library that contains observations collected by the US Environment Protection Agency (EPA)
+#on 38 models of cars.
+
+#Q1. Plot a scatterplot of hwy and displ? 
+#Hints: ggplot, geom_point,
+
+
+#Q2. Using the geom_function, add a layer to your scatterplot to differentiate the 
+#different types of cars, defined by the class variable.
+#Hints: ggplot, geom_point, color 
+
+#Q3. For all displ > 5.5 and hwy > 20, color data in blue
+#Hints: ggplot, geom_point, filter, color
+
+
+#Q4. Using the geom_function, create a smooth line plot to establish the 
+#relationship between displ and hwy.
+#Hints: ggplot, geom_point, geom_smooth
+
+
+
+#Q5. Using the geom_function, add a layer to your scatterplot to create a 
+#smooth line to differentiate the different types of cars, 
+#defined by the class variable.
+#Hints: ggplot, geom_point, geom_smooth
+
+
+#Q6. Using the geom_function, add a layer to your scatterplot to create a 
+#smooth line just for the subcompact cars in the class variable.
+#Hints: ggplot, geom_point, filter
+
+
+
+#Q7. facet_wrap() - facet your plot by single variable
+#Hints: ggplot, geom_point, facet_wrap
+
+
+#Q8. What's wrong with this code?
+ggplot(data = mpg) +geom_point(mapping = aes(x = displ, y = hwy, color = class))
+
+
+#Q9. What's wrong with this code?
+ggplot(data = mpg) + geom_point(mapping = aes(color = class)) + geom_smooth(data = filter(mpg, class == "suv")) 
+
+
+
+#Your Turn
 #Q1. Review your data set mpg
 
 
